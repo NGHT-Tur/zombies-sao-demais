36a27741110333ab2dd5e94f9d79f2c17015dea5
NGHT-Tur <104224519+NGHT-Tur@users.noreply.github.com>
2022-08-10 15:48:10 -0300

    10-08-2022

--- sketch.js
+++ sketch.js
@@ -5,11 +5,23 @@ var joberto;
 var joberto1;
 var joberto2;
 var best;
+var amor;
+var amor1;
+var amor2;
+var amor3;
+var amor4;
+var amor5;
+var picoledesangue=1000;
+var picoledesangue1;
+var joaovitor;
 function preload(){
   fundo=loadImage("./assets/bg.jpeg");
   zombies=loadImage("./assets/zombie.png");
   joberto1=loadAnimation("./assets/shooter_1.png","./assets/shooter_2.png");
   joberto2=loadAnimation("./assets/shooter_3.png");
+  amor3=loadImage("./assets/heart_1.png");
+  amor4=loadImage("./assets/heart_2.png");
+  amor5=loadImage("./assets/heart_3.png");
 }
 
 function setup() {
@@ -19,10 +31,26 @@ joberto.addAnimation("piscadelas", joberto1);
 joberto.scale= 0.35;
 joberto.addAnimation("saladooitavo", joberto2);
 best=new Group();
+amor=createSprite(735,100);
+amor1=createSprite(767,100);
+amor2=createSprite(800,100);
+amor.addImage(amor3);
+amor1.addImage(amor4);
+amor2.addImage(amor5);
+amor.scale=0.3;
+amor1.scale=0.3;
+amor2.scale=0.3;
+amor.visible=false;
+amor1.visible=false;
+picoledesangue1=new Group();
 }
 
 function draw() {
 background(fundo);
+fill("skyblue");
+textSize(19);
+text("repelentes restantes:"+picoledesangue,50,50);
+
       if (keyIsDown(UP_ARROW)) {
         joberto.y -= 10;
       }
@@ -40,10 +68,29 @@ background(fundo);
   }
  if(keyWentDown("space")){
     joberto.changeAnimation("saladooitavo");
+    joaovitor=createSprite(joberto.x,joberto.y-29,10,5);
+    joaovitor.velocityX=20;
+    picoledesangue1.add(joaovitor);
+    picoledesangue-=1;
  }
  if(keyWentUp("space")){
     joberto.changeAnimation("piscadelas");
  }
+ if(best.isTouching(joberto)){
+  for(var jobertomorrelogo=0;jobertomorrelogo<best.length;jobertomorrelogo++){
+    if(best[jobertomorrelogo].isTouching(joberto)){
+      best[jobertomorrelogo].destroy();
+  }
+ }
+}
+if(best.isTouching(picoledesangue1)){
+  for(var zombiemorrelogo=0;zombiemorrelogo<best.length;zombiemorrelogo++){
+    if(best[zombiemorrelogo].isTouching(picoledesangue1)){
+      best[zombiemorrelogo].destroy();
+      picoledesangue1.destroyEach();
+  }
+ }
+}
 drawSprites();
 zombiesmorte();
 }
@@ -56,6 +103,9 @@ zombie.velocityX=-0.5;
 zombie.scale=0.2;
 zombie.lifeTime=1810;
 best.add(zombie);
+zombie.debug=true;
+zombie.setCollider("rectangle",0,0,100,900);
 }
+
 
 }
